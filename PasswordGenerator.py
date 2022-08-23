c74c4fb468157b123042372cff04a75feba15fcf
Dan McGrath <daniel-mcgrath@stlukes.qld.edu.au>
2022-08-23 15:22:12 +1000

    first commit

---
+++ PasswordGenerator.py
@@ -0,0 +1,16 @@
+import requests
+from csv import reader
+
+url = "https://www.dinopass.com/password/strong"
+
+# open file in read mode
+with open('users.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    for row in csv_reader:
+        # row variable is a list that represents a row in csv
+        resp = requests.get(url)
+        password = resp.content.decode("utf-8")
+        user = row[0]
+        print(user + ',' + password)
